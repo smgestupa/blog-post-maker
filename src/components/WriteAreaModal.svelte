ff11e88673898e4fc131419c10945450e6ef146f
Shawn Shawn <laazy.pr00gramming@protonmail.com>
2021-09-23 19:56:58 +0800

    made the modal more responsive based on the screen width

--- src/components/WriteAreaModal.svelte
+++ src/components/WriteAreaModal.svelte
@@ -7,10 +7,20 @@
 
 </style>
 
-<div class="flex hidden justify-center items-center bg-black bg-opacity-50 absolute inset-0" id="overlay">
-    <!-- WRITING MODAL -->
-    <div class="bg-gray-300 py-4 px-4 rounded shadow-xl">
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus earum excepturi veniam! Aperiam fugiat sequi velit vitae. A aut dolores est eveniet excepturi, illo in natus odio pariatur quae repellat!
+<div class="bg-black bg-opacity-50 w-full h-full top-0 left-0 block fixed z-10" id="overlay">
+    <div class="bg-gray-300 w-9/12 p-2 rounded shadow-xl my-10 mx-auto">
+        <!-- MODAL HEADER -->
+        <div class="text-center">
+            <!-- POST TITLE --->
+            <h1>asdasdasd</h1>
+            <!-- /POST TITLE --->
+        </div>
+<!--        &lt;!&ndash; /MODAL HEADER &ndash;&gt;-->
+<!--        &lt;!&ndash; MODAL WRITE/DISPLAY AREA &ndash;&gt;-->
+<!--        <div class="flex">-->
+<!--&lt;!&ndash;            <textarea name="" id="asdasdasd" cols="30" rows="10" style="max-width: 25%"></textarea>&ndash;&gt;-->
+<!--&lt;!&ndash;            <textarea name="" id="wewew" cols="30" rows="10"></textarea>&ndash;&gt;-->
+<!--        </div>-->
+<!--        &lt;!&ndash; /MODAL WRITE/DISPLAY AREA &ndash;&gt;-->
     </div>
-    <!-- /WRITING MODAL -->
 </div>
