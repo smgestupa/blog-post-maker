d93b3e9ec139b207d2401388bf7af39d85061d2c
Shawn Shawn <laazy.pr00gramming@protonmail.com>
2021-09-24 18:16:04 +0800

    removed stores, wasn't really needed

--- src/components/WriteAreaModal.svelte
+++ src/components/WriteAreaModal.svelte
@@ -1,19 +1,53 @@
 <script lang="ts">
     import marked from 'marked';
-    import { text } from '$stores/stores.ts'
+    let textArea = `Please write anything you want here!`;
 
-    $: markdown = marked( `${ $text }` );
+    marked.setOptions({
+        breaks: true
+    });
+
+    $: markdown = marked( textArea );
 </script>
 
-<div class="bg-black bg-opacity-50 w-full h-full top-0 left-0 block fixed z-10" id="overlay">
-    <div class="bg-gray-300 w-7/12 p-2 rounded shadow-xl my-10 mx-auto">
+<style>
+    .modal {
+        @apply bg-black bg-opacity-50 w-full h-full top-0 left-0 block fixed z-10;
+    }
+
+    .inner-modal {
+        @apply bg-gray-300 w-7/12 p-2 rounded shadow-xl my-10 mx-auto;
+    }
+
+    .markdown-write {
+        @apply w-3/5 border-solid border-black;
+    }
+
+    .markdown-write textarea {
+        @apply w-full h-full pt-1 px-2 border-black bg-gray-700 text-gray-200 resize-none;
+        height: 85vh;
+        border-width: 1px;
+    }
+
+    .markdown-preview {
+        @apply w-3/5 border-solid border-black bg-white overflow-auto break-words;
+        height: 85vh;
+        border-width: 1px;
+    }
+
+    .markdown-preview div {
+        @apply w-full pt-1 px-2;
+    }
+</style>
+
+<div class="modal" id="overlay">
+    <div class="inner-modal">
         <div class="flex items-start content-evenly w-full">
-            <div class="w-3/5 border-solid border-black " style="height: 85vh; border-width: 1px;">
-                <textarea class="w-full h-full bg-gray-700 text-gray-200 resize-none" bind:value={ $text }></textarea>
+            <div class="markdown-write">
+                <textarea bind:value={ textArea }></textarea>
             </div>
 
-            <div class="w-3/5 border-solid border-black bg-white overflow-auto break-words" style="height: 85vh; border-width: 1px;">
-                <div class="w-full px-8">{@html markdown}</div>
+            <div class="markdown-preview">
+                <div>{ @html markdown }</div>
             </div>
         </div>
     </div>
