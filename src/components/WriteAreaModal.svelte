eecc04cb42dc43530724344f81085902402d5206
Shawn Shawn <laazy.pr00gramming@protonmail.com>
2021-09-25 15:17:56 +0800

    made the preview area more round so scrollbars cannot turn the borders into square anymore + added a default text for the write area

--- src/components/WriteAreaModal.svelte
+++ src/components/WriteAreaModal.svelte
@@ -2,7 +2,29 @@
     import { fade } from 'svelte/transition';
     import { showModal } from '$stores/stores.js';
     import marked from 'marked';
-    let textArea: string = ``;
+    let textArea: string = `# H1 heading
+
+## H2 heading
+
+### H3 heading
+
+--------
+
+**bold text**
+
+*italicized text*
+
+--------
+
+1. First item
+2. Second item
+3. Third item
+
+- First item
+- Second item
+- Third item
+
+[Svelte](https://svelte.dev/)`;
 
     marked.setOptions({
         breaks: true
@@ -15,7 +37,8 @@
 
 <style>
     .modal {
-        @apply bg-gray-300 w-9/12 p-2 rounded-lg shadow-xl my-10 mx-auto;
+        @apply bg-gray-300 w-9/12 p-2 rounded-lg shadow-xl mx-auto;
+        margin-top: 4vh;
     }
 
     .modal-buttons {
@@ -51,38 +74,40 @@
     }
 
     .markdown {
-        @apply flex items-start content-evenly w-full p-5;
+        @apply flex w-full p-5;
     }
 
     .markdown-write {
-        @apply w-3/5 border-solid border-black;
+        @apply w-3/5 flex items-center rounded-l-lg overflow-hidden;
     }
 
     .markdown-write textarea {
-        @apply text-xl w-full h-full pt-3 pb-2 pl-3 pr-2 rounded-l-lg border-black bg-gray-700 text-gray-200 outline-none resize-none;
-        height: 75vh;
-        border-width: 1px;
+        @apply text-xl w-full pt-3 pb-2 pl-3 pr-2 text-gray-200 outline-none resize-none overflow-auto;
+        height: 70vh;
+        background-color: #36393f;
+        color: white;
     }
 
     .markdown-preview {
-        @apply w-3/5 rounded-r-lg border-solid border-black bg-white overflow-auto break-all;
-        height: 75vh;
-        border-width: 1px;
+        @apply w-3/5 rounded-r-lg overflow-hidden break-all;
     }
 
     .markdown-preview div {
-        @apply w-full pt-3 pb-2 pl-3 pr-2;
+        @apply w-full pt-3 pb-2 pl-3 pr-2 overflow-auto bg-white max-w-none;
+        height: 70vh;
+        background-color: white;
+        color: #36393f;
     }
 </style>
 
-{ #if $showModal }
-    <div class="bg-black bg-opacity-50 w-full h-full top-0 left-0 absolute z-10" id="overlay" transition:fade={ { duration: 300 } }>
+{ #if !$showModal }
+    <div class="bg-black bg-opacity-50 w-full h-full top-0 left-0 fixed z-10" id="overlay" transition:fade={ { duration: 300 } }>
         <div class="modal">
             <!-- MODAL BUTTONS -->
             <div class="modal-buttons">
-                <button class="close-button" id="close-modal" on:click|preventDefault={ modalClose }>Close Editor</button>
+                <button class="close-button" id="close-modal" on:click|preventDefault={ modalClose }>Close editor</button>
                 <button class="close-button-icon">
-                    <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" aria-hidden="true" role="img" width="1em" height="1em" preserveAspectRatio="xMidYMid meet" viewBox="0 0 1024 1024"><path d="M512 64C264.6 64 64 264.6 64 512s200.6 448 448 448s448-200.6 448-448S759.4 64 512 64zm165.4 618.2l-66-.3L512 563.4l-99.3 118.4l-66.1.3c-4.4 0-8-3.5-8-8c0-1.9.7-3.7 1.9-5.2l130.1-155L340.5 359a8.32 8.32 0 0 1-1.9-5.2c0-4.4 3.6-8 8-8l66.1.3L512 464.6l99.3-118.4l66-.3c4.4 0 8 3.5 8 8c0 1.9-.7 3.7-1.9 5.2L553.5 514l130 155c1.2 1.5 1.9 3.3 1.9 5.2c0 4.4-3.6 8-8 8z" fill="currentColor"/></svg>
+                    <svg xmlns="http://www.w3.org/2000/svg" aria-hidden="true" role="img" width="1em" height="1em" preserveAspectRatio="xMidYMid meet" viewBox="0 0 1024 1024"><path d="M512 64C264.6 64 64 264.6 64 512s200.6 448 448 448s448-200.6 448-448S759.4 64 512 64zm165.4 618.2l-66-.3L512 563.4l-99.3 118.4l-66.1.3c-4.4 0-8-3.5-8-8c0-1.9.7-3.7 1.9-5.2l130.1-155L340.5 359a8.32 8.32 0 0 1-1.9-5.2c0-4.4 3.6-8 8-8l66.1.3L512 464.6l99.3-118.4l66-.3c4.4 0 8 3.5 8 8c0 1.9-.7 3.7-1.9 5.2L553.5 514l130 155c1.2 1.5 1.9 3.3 1.9 5.2c0 4.4-3.6 8-8 8z" fill="currentColor"/></svg>
                 </button>
             </div>
             <!-- /MODAL BUTTONS -->
@@ -100,7 +125,7 @@
                 <!-- /MARKDOWN WRITE AREA -->
                 <!-- MARKDOWN PREVIEW AREA -->
                 <div class="markdown-preview">
-                    <div class="prose prose-lg max-w-none">
+                    <div class="prose prose-sm md:prose-lg">
                         { @html markdown }
                     </div>
                 </div>
